9a9c26392201ecb193141c577b6579c9cdf198dc
Rahul Chandra <rahulshaku2@gmail.com>
2023-03-13 20:27:00 -0400

    General Updates and package additions file an issue if you want a new ISO

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL=https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.1.tar.xz
+URL=https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.6.tar.xz
 TAR=$(echo $URL | sed -r 's|(.*)/||')
 DIR=$(echo $TAR | sed 's|.tar.*||g')
 KERNEL_VERSION=$(echo $DIR | sed 's/linux-//')
