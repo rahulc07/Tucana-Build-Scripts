e499b70d808764e02a66517559842ead5f4f9bd1
Rahul Chandra <rahulshaku2@gmail.com>
2023-11-10 16:44:41 -0500

    Fixes

---
+++ cinnamon/build-cinnamon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir -p /logs/cinnamon-logs 
+
+build() {
+   bash $1 &> /logs/cinnamon-logs/$1-$(date '+%m-%d-%Y').log
+   if [[ ! $? == 0 ]]; then
+      echo $1 >> /logs/cinnamon-logs/failed.txt
+   fi
+}
+build "python3-xapp"
+build "cjs"
+build "cinnamon-desktop"
+build "cinnamon-settings-daemon"
+build "cinnamon-menus"
+build "cinnamon-control-center"
+build "cinnamon-session"
+build "cinnamon-screensaver"
+build "muffin"
+build "nemo"
