2ef9b51f50f84fe14200e4668f31667d122876aa
Rahul Chandra <rahul@projecteclipse.org>
2024-08-01 02:00:51 -0400

    Update version

--- kde/build_kde.sh
+++ kde/build_kde.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 set -e
+# Building Depends
+bash ecm
+bash phonon
+#bash phonon-backend-vlc
+bash polkit-qt
+bash plasma-wayland-protocols
 
+# Plasma5-limited
+cd plasma5-limited
+bash -e build_plasma5_limited.sh
+cd ..
+# KF6
+cd kf6
+bash -e build_kf6.sh
+cd ..
+# Desktop
+cd plasma
+bash -e build_plasma.sh
+cd ..
+# Apps
+cd plasma-apps
+bash -e build_plasma_apps.sh
--- kde/kf6/build_kf6.sh
+++ kde/kf6/build_kf6.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 bash kirigami
 bash ktexttemplate
 bash kuserfeedback
