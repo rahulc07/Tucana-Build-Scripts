3082356fea4afb8b37df49156ed95437aed4b480
Rahul Chandra <rahul@projecteclipse.org>
2024-08-01 02:04:57 -0400

    changes

---
+++ kde/plasma-apps/build_plasma_apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo mercury-install cpio p7zip unzip unrar zip libarchive mlt v4l-utils xapian exiv2 boost libtiff poppler libraw kimageannotator lcms
+bash -e ark
+bash -e dolphin
+bash -e kdenlive
+bash -e khelpcenter
+bash -e konsole
+bash -e libkexiv2
+bash -e plasma-activites
+bash -e okular
+bash -e libkdcraw
+bash -e gwenview
+bash -e spectacle
