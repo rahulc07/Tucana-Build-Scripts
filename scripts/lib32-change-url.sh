53168fc08152b0d26d148d7f78e8c9e5e89d90f6
Rahul Chandra <rahul@projecteclipse.org>
2024-08-07 04:26:19 +0000

    stuff

--- scripts/lib32-change-url.sh
+++ scripts/lib32-change-url.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 for i in $(find -maxdepth 1 -type f | sed 's!.*/!!' | sed 's/lib32-//g'); do 
-   if [[ -f ../$i ]]; then
-        LINES=$(cat ../$i | grep '\S' | head -n6)
-        cat lib32-$i | tail -n +6
- 
-   elif [[ -f ../base/$i ]]; then
-        LINES=$(cat ../base/$i | grep '\S' | head -n8)
-        cat lib32-$i | tail -n +8
+   echo $i:
+   if [[ -f ../$i ]] && ! cat lib32-$i | grep -q 'PKG_VER'; then
+        URLLINE=$(cat ../$i | grep 'URL=')
+	echo $URLLINE
+	cat ../$i | grep 'MAJOR=' > /dev/null
+	if [[ $? == 0 ]]; then
+           MAJORLINE=$(cat ../$i | grep 'MAJOR=')
+	   echo $MAJORLINE
+	fi
+	PKGVERLINE=$(cat ../$i | grep 'PKG_VER=')
+	echo $PKGVERLINE
+   elif [[ -f ../base/$i ]] && ! cat lib32-$i | grep -q 'PKG_VER'; then
+        URLLINE=$(cat ../base/$i | grep 'URL=')
+	echo $URLLINE
+	cat ../base/$i | grep 'MAJOR=' > /dev/null
+	if [[ $? == 0 ]]; then
+           MAJORLINE=$(cat ../base/$i | grep 'MAJOR=')
+	   echo $MAJORLINE
+	fi
+	PKGVERLINE=$(cat ../base/$i | grep 'PKG_VER=')
+	echo $PKGVERLINE
    fi
+   echo ""
    
 done
