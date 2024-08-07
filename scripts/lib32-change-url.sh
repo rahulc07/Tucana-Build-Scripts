e858777fc3ab8e9a6e3bb2be985527ad75169c07
Rahul Chandra <rahul@projecteclipse.org>
2024-08-07 05:27:29 +0000

    lib32 currency

--- scripts/lib32-change-url.sh
+++ scripts/lib32-change-url.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 for i in $(find -maxdepth 1 -type f | sed 's!.*/!!' | sed 's/lib32-//g'); do 
    echo $i:
-   if [[ -f ../$i ]] && ! cat lib32-$i | grep -q 'PKG_VER'; then
+   if [[ -f ../$i ]]; then
         URLLINE=$(cat ../$i | grep 'URL=')
 	echo $URLLINE
+	MAJORLINE=" "
 	cat ../$i | grep 'MAJOR=' > /dev/null
 	if [[ $? == 0 ]]; then
            MAJORLINE=$(cat ../$i | grep 'MAJOR=')
 	   echo $MAJORLINE
 	fi
 	PKGVERLINE=$(cat ../$i | grep 'PKG_VER=')
+	if cat lib32-$i | grep -v 'PKG_VER='; then
+	    sed -i 's/PKG_VER=.*//'lib32-$i
+	fi
 	echo $PKGVERLINE
-   elif [[ -f ../base/$i ]] && ! cat lib32-$i | grep -q 'PKG_VER'; then
+	sed -i "s~URL=.*~${PKGVERLINE}\n${MAJORLINE}\n${URLLINE}~" lib32-$i
+   elif [[ -f ../base/$i ]]; then
         URLLINE=$(cat ../base/$i | grep 'URL=')
 	echo $URLLINE
-	cat ../base/$i | grep 'MAJOR=' > /dev/null
+	MAJORLINE=" "
+	cat ../base/$i | grep -E 'MAJOR=|MINOR=' > /dev/null
 	if [[ $? == 0 ]]; then
-           MAJORLINE=$(cat ../base/$i | grep 'MAJOR=')
+           MAJORLINE=$(cat ../base/$i | grep 'MINOR=|MAJOR=')
 	   echo $MAJORLINE
 	fi
 	PKGVERLINE=$(cat ../base/$i | grep 'PKG_VER=')
+	if cat lib32-$i | grep -v 'PKG_VER='; then
+	    sed -i 's/PKG_VER=.*//' lib32-$i
+	fi
 	echo $PKGVERLINE
+	sed -i "s~URL=.*~${PKGVERLINE}\n${MAJORLINE}\n${URLLINE}~" lib32-$i
    fi
    echo ""
    
