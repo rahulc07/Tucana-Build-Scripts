eaa17962ce4c7a6247b2fccaf94836a6cc65680b
Rahul Chandra <rahul@projecteclipse.org>
2024-09-24 05:01:31 +0000

    Don't delete module symver

--- build_kernel.sh
+++ build_kernel.sh
@@ -32,7 +32,7 @@ make INSTALL_MOD_PATH=../linux-tucana/usr modules_install
 
 sudo cp arch/x86/boot/bzImage ../linux-tucana/boot/vmlinuz-$KERNEL_VERSION-tucana
 sudo cp .config ../linux-tucana/boot/config-tucana
-sudo rm -r block certs/ crypto/ Documentation/ drivers/ fs/ init/ ipc/ kernel/ lib/ LICENSES/ mm/ MAINTAINERS  modules.* Module.symvers net/ samples/ security/ sound/ usr/ virt/ vmlinux*
+sudo rm -r block certs/ crypto/ Documentation/ drivers/ fs/ init/ ipc/ kernel/ lib/ LICENSES/ mm/ MAINTAINERS  modules.* net/ samples/ security/ sound/ usr/ virt/ vmlinux*
 cd ../linux-tucana
 echo "cd /boot" > postinst
 echo "mkinitramfs $KERNEL_VERSION-tucana" >> postinst
